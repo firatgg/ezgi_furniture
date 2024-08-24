bff3cae5458b4e3caacf158480db9d0dfc9d905f
Fırat Gültekin <frt.gltkn131@gmail.com>
2024-08-24 13:34:09 +0300

    file structure created

---
+++ ezgi_mobilya/ezgi_mobilya/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using ezgi_mobilya.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ezgi_mobilya.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
